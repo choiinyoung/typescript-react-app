77df24e99ab46ca02e7a8863abbd58f48609c876
choiinyoung <choi121100@naver.com>
2023-04-28 13:46:49 +0900

    0428_Study
    0428_Study

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,19 +1,33 @@
+// import React, { useRef, useState } from 'react'
+
+// export default function Input() {
+//   const [string,setString] = useState<string>("")
+//   const inputRef = useRef<HTMLInputElement>(null);
+
+//   const handleChange = ():void =>{
+//     if(inputRef.current !== null) setString(inputRef.current.value);
+//   };
+
+//   return <>
+//       <h1>{string}</h1>
+//       <input 
+//         ref={inputRef} 
+//         onChange={handleChange}
+//       />
+//     </>
+  
+// }
+
 import React, { useRef, useState } from 'react'
 
 export default function Input() {
-  const [string,setString] = useState<string>("")
+  const [string,setString] = useState<string>("");
   const inputRef = useRef<HTMLInputElement>(null);
-
-  const handleChange = ():void =>{
-    if(inputRef.current !== null) setString(inputRef.current.value);
-  };
-
-  return <>
-      <h1>{string}</h1>
-      <input 
-        ref={inputRef} 
-        onChange={handleChange}
-      />
+  return (
+    <>
+      <h1> {string}</h1>
+      <input ref={inputRef} onChange={():void=>setString(inputRef.current.value)}/>
     </>
+  );
   
 }
